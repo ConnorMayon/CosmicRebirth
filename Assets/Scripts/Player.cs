1c04ca21e89af9c80c89c3f96d9b73caca094ec7
qejmc <35905481+qejmc@users.noreply.github.com>
2023-11-20 17:41:42 -0700

    Added collision detection and killing enemies

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Only trigger when jumped on - uses offset of 2
+        if (collision.gameObject.CompareTag("Enemy") && (transform.position.y > (collision.transform.position.y + 2f)))
+        {
+            Debug.Log("Collision detected! Yes");
+
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
