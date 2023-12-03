40624a9d23e7e97fdbc9636f1427f976710ed43a
niklaskariniemi <niklaskariniemi@gmail.com>
2023-12-02 17:42:09 -0700

    added a go back button on the tips scene

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // function to start the game
     public void StartGame()
     {
@@ -27,6 +15,14 @@ public class Main : MonoBehaviour
     public void GoToTips()
     {
         SceneManager.LoadScene("TipsScene");
+    
+    
+    }
+
+    // function to that goes to the start scene
+    public void LoadStart()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 
 }
